e688fb72f3c13fd34e1de9eb886a62927b41c309
ziad amr <ziad.amr97534@gmail.com>
2025-02-23 19:35:57 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const { ActivityType } = require("discord.js");
 
 client.once("ready", () => {
-  console.log(`bot is online! ${client.user.tag}!`);
-  console.log(`Code by 72.20`);
+  console.log(`bot is online ${client.user.tag} ${client.username}`);
+  console.log(`Code by @72.20`);
   console.log(`@discord.gg/witon`)
 
 client.user.setActivity('', {
-    typ: 'PLAYING', // ممكن تغير الحالة من هنا WATCHING STREAMING COMPETING LISTENING
+    typ: 'PLAYING', //  WATCHING STREAMING COMPETING LISTENING
   });
     const randomActivity = activities[Math.floor(Math.random() * activities.length)];
 
-    client.user.setActivity(randomActivity, { type: 'PLAYING' }); // غيرها من هنا ولازم تكون متابقه للي فوق
+    client.user.setActivity(randomActivity, { type: 'PLAYING' }); // 
   }, 10000);
 });
-client.login('توكن بوتك')
+client.on('error', (error) => {
+  console.error('حدثت مشكله توجه الى سيرفر الدعم ومنشن @72.20, error);
+});
+console.log('
+Wick on top 
+discord.gg/witon >>programming
+discord.gg/wick-c >>community
+discord.gg/wick-s >>store
+discord.gg/f3ayaD6q >>wicks bot support
+')
+client.login('توكن بوتك') 
+                if (presence.activities.length > 0) {
+                    console.log(chalk.magentaBright(`the activity of your bot is ${presence.activities[0].name}"`));
